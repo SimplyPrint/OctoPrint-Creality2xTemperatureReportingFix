446fcf910e9d885cdb63f701718ae59a6c281f95
Albert Møller Nielsen <albert@mollernielsen.dk>
2020-09-07 19:15:34 +0200

    Version increment before release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ plugin_package = "octoprint_ender3v2tempfix"
 plugin_name = "Ender-3 v2 Temperature Fix"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.0.1"
+plugin_version = "0.0.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
